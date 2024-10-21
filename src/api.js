f05da929b5af28c1af285274ccebd3978cde3428
lavanyapetchetti <99505509+lavanyapetchetti@users.noreply.github.com>
2024-10-21 16:17:57 -0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ try {
     config = yaml.load(fileContents);
 } catch (e) {
     console.error(`Error reading config file: ${e.message}`);
-    throw e;
+    throw e; // Exit if configuration cannot be loaded
 }
 
 // Function to make GET requests
@@ -25,7 +25,9 @@ const get = async (url, additionalHeaders = {}) => {
         throw error; // Rethrow the error to handle it in the calling function
     }
 };
+// Future functions can be added here, e.g., POST, PUT, DELETE methods
 
 export default {
     get,
+    // You can add more methods here in the future
 };
